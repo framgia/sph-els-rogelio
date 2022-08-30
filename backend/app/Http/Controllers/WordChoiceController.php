8d8c7596454cb44a9b893d7d873da1dbecfcd6b9
Rogelio John Oliverio <rogeliojohn.oliverio@sun-asterisk.com>
2022-08-30 13:46:32 +0800

    Admin-Create Words and Choices [BE]

--- backend/app/Http/Controllers/WordChoiceController.php
+++ backend/app/Http/Controllers/WordChoiceController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Lesson;
+use App\Models\WordQuestion;
 
 class WordChoiceController extends Controller
 {
     public function index($id)
     {
         return Lesson::where('id',$id)->with('words.choices')->first();
+    }
+    public function store(Request $request, $id)
+    {
+        $word=WordQuestion::create([
+            'lesson_id'=>$id,
+            'word'=>$request->word,
+            'usage'=>$request->usage,
+        ]);
+        $word=$word->choices()->createMany($request->choices);
+        if(!$word){
+            return response()->json(['status' => false, 'data'=>null, 'message' => 'Cannot create new word.', 'errors' => 'Create Error'], 500);
+        }
+        return response()->json([
+            'status' => true,
+            'data'=>$word, 
+            'message' => 'New word successfully created.', 
+            'errors' => null
+        ],200); 
     }
 }
