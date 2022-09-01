905aaaf96e26cb1363a28ac4ed83443431e006a3
Rogelio John Oliverio <110364637+rogeliojohnoliverio@users.noreply.github.com>
2022-09-01 10:11:24 +0800

    [SELS-TASK] Admin-Display Category [FE] (#7)
    * Registration [FE]
    
    * Registration [BE]
    
    * Login [FE]
    
    * Login [BE]
    
    * Page Layout [FE]
    
    * Admin-Display Category [FE]
    
    * Configured prettier for github indention
    
    * Added dummy data in utils

--- frontend/src/pages/AdminLessonsPage.js
+++ frontend/src/pages/AdminLessonsPage.js
@@ -1,11 +1,59 @@
 import React from "react";
 import withAdminProtection from "../utilities/withAdminProtection";
 import PageLayout from "./components/layout/PageLayout";
+import DataTable from "react-data-table-component";
+import Button from "./components/button/Button";
+import { customStyles } from "./components/datatable/datatable";
+import { dummyLessons } from "../utilities/dummyData";
 
 const AdminLessonsPage = () => {
+  const columns = [
+    {
+      name: "Title",
+      selector: (row) => row.title,
+      wrap: true,
+      width: "25%",
+    },
+    {
+      name: "Description",
+      selector: (row) => row.description,
+      wrap: true,
+      style: {},
+      width: "50%",
+    },
+    {
+      name: "Actions",
+      cell: (row) => (
+        <div className="w-100 d-flex gap-1">
+          <Button
+            className="btn btn-primary btn-sm"
+            label="Add word"
+            isValid={true}
+          />
+          <Button
+            className="btn btn-warning btn-sm"
+            label="Edit"
+            isValid={true}
+          />
+          <Button
+            className="btn btn-danger btn-sm"
+            label="Delete"
+            isValid={true}
+          />
+        </div>
+      ),
+    },
+  ];
+
   return (
     <PageLayout pageTitle={"Admin Dashboard"}>
-      <div>Admin</div>
+      <DataTable
+        title="Lessons/Categories"
+        columns={columns}
+        data={dummyLessons}
+        customStyles={customStyles}
+        pagination
+      />
     </PageLayout>
   );
 };
