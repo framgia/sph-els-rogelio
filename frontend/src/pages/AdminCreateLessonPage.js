964674087e581e3ae663e9b27e1dd74aa59c6271
Rogelio John Oliverio <rogeliojohn.oliverio@sun-asterisk.com>
2022-09-01 11:44:27 +0800

    Configured prettier indention github

--- frontend/src/pages/AdminCreateLessonPage.js
+++ frontend/src/pages/AdminCreateLessonPage.js
@@ -8,89 +8,89 @@ import { toast } from "react-toastify";
 import withAdminProtection from "../utilities/withAdminProtection";
 
 const AdminCreateLessonPage = () => {
-	const [createLesson, { isLoading }] = useCreateLessonMutation();
-	const handleFormSubmit = async () => {
-		try {
-			const res = await createLesson(values).unwrap();
-			toast.success(res.message);
-			resetForm();
-		} catch (error) {
-			if (error && error.status === 500) {
-				toast.error(error.message);
-			} else {
-				toast.error(error);
-			}
-		}
-	};
-	const {
-		handleChange,
-		handleSubmit,
-		handleBlur,
-		resetForm,
-		values,
-		errors,
-		isValid,
-		touched,
-	} = useFormik({
-		initialValues: { title: "", description: "" },
-		enableReinitialize: true,
-		validationSchema: lessonValidationSchema,
-		onSubmit: handleFormSubmit,
-	});
-	return (
-		<PageLayout pageTitle={"Admin Dashboard | Create Lesson"}>
-			<div className="m-auto d-flex justify-content-center align-items-center w-100 h-100">
-				<form className="w-50 mt-5">
-					<h3>Add Lesson/Category</h3>
-					<div className="form-floating mb-2 my-3">
-						<input
-							type="text"
-							onChange={handleChange("title")}
-							onBlur={handleBlur("title")}
-							value={values.title}
-							className={`form-control ${
-								errors.title && touched.title ? "border-danger" : ""
-							}`}
-							id="floatingInput"
-							placeholder="Title"
-						/>
-						<label htmlFor="floatingInput">Title</label>
-						{errors.title && touched.title && (
-							<span className="error small text-danger">{errors.title}</span>
-						)}
-					</div>
-					<div className="form-floating">
-						<textarea
-							onChange={handleChange("description")}
-							onBlur={handleBlur("description")}
-							value={values.description}
-							className={`form-control ${
-								errors.description && touched.description ? "border-danger" : ""
-							}`}
-							id="floatingInput"
-							placeholder="Description"
-							cols="30"
-							rows="10"
-							style={{ height: "100%" }}
-						/>
-						<label htmlFor="floatingInput">Description</label>
-						{errors.description && touched.description && (
-							<span className="error small text-danger">
-								{errors.description}
-							</span>
-						)}
-					</div>
-					<Button
-						className={"w-100 mt-3 btn btn-lg btn-primary"}
-						isValid={isValid}
-						isLoading={isLoading}
-						handleClick={handleSubmit}
-						label={"Add Lesson"}
-					/>
-				</form>
-			</div>
-		</PageLayout>
-	);
+  const [createLesson, { isLoading }] = useCreateLessonMutation();
+  const handleFormSubmit = async () => {
+    try {
+      const res = await createLesson(values).unwrap();
+      toast.success(res.message);
+      resetForm();
+    } catch (error) {
+      if (error && error.status === 500) {
+        toast.error(error.message);
+      } else {
+        toast.error(error);
+      }
+    }
+  };
+  const {
+    handleChange,
+    handleSubmit,
+    handleBlur,
+    resetForm,
+    values,
+    errors,
+    isValid,
+    touched,
+  } = useFormik({
+    initialValues: { title: "", description: "" },
+    enableReinitialize: true,
+    validationSchema: lessonValidationSchema,
+    onSubmit: handleFormSubmit,
+  });
+  return (
+    <PageLayout pageTitle={"Admin Dashboard | Create Lesson"}>
+      <div className="m-auto d-flex justify-content-center align-items-center w-100 h-100">
+        <form className="w-50 mt-5">
+          <h3>Add Lesson/Category</h3>
+          <div className="form-floating mb-2 my-3">
+            <input
+              type="text"
+              onChange={handleChange("title")}
+              onBlur={handleBlur("title")}
+              value={values.title}
+              className={`form-control ${
+                errors.title && touched.title ? "border-danger" : ""
+              }`}
+              id="floatingInput"
+              placeholder="Title"
+            />
+            <label htmlFor="floatingInput">Title</label>
+            {errors.title && touched.title && (
+              <span className="error small text-danger">{errors.title}</span>
+            )}
+          </div>
+          <div className="form-floating">
+            <textarea
+              onChange={handleChange("description")}
+              onBlur={handleBlur("description")}
+              value={values.description}
+              className={`form-control ${
+                errors.description && touched.description ? "border-danger" : ""
+              }`}
+              id="floatingInput"
+              placeholder="Description"
+              cols="30"
+              rows="10"
+              style={{ height: "100%" }}
+            />
+            <label htmlFor="floatingInput">Description</label>
+            {errors.description && touched.description && (
+              <span className="error small text-danger">
+                {errors.description}
+              </span>
+            )}
+          </div>
+          <Button
+            className={"w-100 mt-3 btn btn-lg btn-primary"}
+            isValid={isValid}
+            isLoading={isLoading}
+            handleClick={handleSubmit}
+            label={"Add Lesson"}
+          />
+        </form>
+      </div>
+    </PageLayout>
+  );
 };
 
 export default withAdminProtection(AdminCreateLessonPage);
--- frontend/src/store/lessonsSlice.js
+++ frontend/src/store/lessonsSlice.js
@@ -1,29 +1,29 @@
 import { apiSlice } from "./apiSlice";
 
 export const extendedApiSlice = apiSlice.injectEndpoints({
-	endpoints: (builder) => ({
-		getLessons: builder.query({
-			query: () => ({
-				url: "/lessons",
-				method: "GET",
-			}),
-			providesTags: (result, error, arg) =>
-				result
-					? [
-							...result.map(({ id }) => ({ type: "Lesson", id })),
-							{ type: "Lesson", id: "LIST" },
-					  ]
-					: [{ type: "Lesson", id: "LIST" }],
-		}),
-		createLesson: builder.mutation({
-			query: (data) => ({
-				url: "/lessons",
-				method: "POST",
-				data: data,
-			}),
-			invalidatesTags: [{ type: "Lesson", id: "LIST" }],
-		}),
-	}),
+  endpoints: (builder) => ({
+    getLessons: builder.query({
+      query: () => ({
+        url: "/lessons",
+        method: "GET",
+      }),
+      providesTags: (result, error, arg) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Lesson", id })),
+              { type: "Lesson", id: "LIST" },
+            ]
+          : [{ type: "Lesson", id: "LIST" }],
+    }),
+    createLesson: builder.mutation({
+      query: (data) => ({
+        url: "/lessons",
+        method: "POST",
+        data: data,
+      }),
+      invalidatesTags: [{ type: "Lesson", id: "LIST" }],
+    }),
+  }),
 });
 
 export const { useGetLessonsQuery, useCreateLessonMutation } = extendedApiSlice;
