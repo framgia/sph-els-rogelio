c727a9e4669153caef7025390bc5459cfa120df5
Rogelio John Oliverio <110364637+rogeliojohnoliverio@users.noreply.github.com>
2022-09-05 14:45:33 +0800

    [SELS-TASK] Admin-Display Words and Choices [BE] (#16)
    [SELS-TASK] Admin-Delete Category [FE]
    
    [SELS-TASK] Admin-Delete Category [BE]
    
    [SELS-TASK] Admin-Display Words and Choices [FE]
    
    [SELS-TASK] Admin-Display Words and Choices [BE]

---
+++ frontend/src/store/wordsChoicesSlice.js
@@ -0,0 +1,15 @@
+import { apiSlice } from "./apiSlice";
+
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getWordsChoices: builder.query({
+      query: (id) => ({
+        url: `/lessons/${id}/words`,
+        method: "GET",
+      }),
+      providesTags: (result, error, id) => [{ type: "WordChoice", id }],
+    }),
+  }),
+});
+
+export const { useGetWordsChoicesQuery } = extendedApiSlice;
