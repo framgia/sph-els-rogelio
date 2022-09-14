c6bf4162f54dda20d85afcf9ab208db4a6ffffb1
Rogelio John Oliverio <110364637+rogeliojohnoliverio@users.noreply.github.com>
2022-09-14 09:48:18 +0800

    [SELS-TASK] User Profile [FE] (#33)
    [SELS-TASK] Admin-Delete Category [FE]
    
    [SELS-TASK] Admin-Delete Category [BE]
    
    [SELS-TASK] Admin-Display Words and Choices [FE]
    
    [SELS-TASK] Admin-Display Words and Choices [BE]
    
    [SELS-TASK] Admin-Create Words and Choices [FE]
    
    [SELS-TASK] Admin-Create Words and Choices [BE]
    
    [SELS-TASK] Admin-Update Words and Choices [FE]
    
    [SELS-TASK] Admin-Update Words and Choices [BE]
    
    [SESLS-TASK] Admin-Delete Words and Choices [FE]
    
    [SELS-TASK] Admin-Delete Words and Choices [BE]
    
    [SELS-TASK] User-Display Lessons [FE]
    
    [SELS-TASK] User-Display Lessons [BE]
    
    [SELS-TASK] User-Filter Lessons [FE]
    
    [SELS-TASK] User-Show Quiz Questionnaire [FE]
    
    [SELS-TASK] User-Show Quiz Questionnaire [BE]
    
    [SELS-TASK] User-Validate Quiz Result [BE]
    
    [SELS-TASK] User-Show Quiz Result [FE]
    
    [SELS-TASK] User-Show Quiz Result [BE]
    
    [SELS-TASK] User-Dashboard [FE]
    
    [SELS-TASK] User-Dashboard [BE]
    
    [SELS-TASK] User Profile [FE]

--- frontend/src/pages/UserProfilePage.js
+++ frontend/src/pages/UserProfilePage.js
@@ -1,93 +1,75 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { Link } from "react-router-dom";
-import { useGetUserDashboardQuery } from "../store/dashboardSlice";
-import withUserProtection from "../utilities/withUserProtection";
-import ErrorPage from "./components/error/ErrorPage";
+import Button from "./components/button/Button";
 import PageLayout from "./components/layout/PageLayout";
-import DataLoading from "./components/loading/DataLoading";
 
-const UserDashboard = () => {
-  const {
-    data: userDashboard,
-    isLoading,
-    isError,
-    isSuccess,
-    isFetching,
-  } = useGetUserDashboardQuery({}, { refetchOnMountOrArgChange: true });
-
-  const countLearnedWords = (lessons) => {
-    let count = 0;
-    lessons.forEach((lesson) => {
-      count += lesson.learned_words.length;
-    });
-    return count;
-  };
-  let output;
-  if (isLoading || isFetching) {
-    output = <DataLoading />;
-  }
-  if (isError) {
-    output = (
-      <ErrorPage
-        errorStatus={404}
-        errorType={"User not found"}
-        errorMessage={"The user you are looking for is not found."}
-      />
-    );
-  }
-  if (isSuccess && !isFetching) {
-    output = (
+const UserProfilePage = () => {
+  return (
+    <PageLayout pageTitle={"User Profile"}>
       <Row className="p-5 h-100">
         <Col className="h-100">
-          <h4 className="mb-3">Dashboard</h4>
           <div className="card p-3">
-            <div className="d-flex align-items-center justify-content-center ">
+            <div className="text-center">
               <img
-                src={`${process.env.REACT_APP_IMAGES_URL}/${userDashboard.avatar}`}
-                width="100"
+                src={"https://picsum.photos/200/300"}
+                width="150"
                 alt="user-profile"
-                height="100"
+                height="150"
                 style={{ objectFit: "cover" }}
                 className="rounded-circle me-2 border p-1 border-3 border-success"
               />
-              <div>
-                <h5 className="d-inline">{userDashboard.name}</h5>
-                <span className="small d-block">{userDashboard.email}</span>
+              <div className="mt-3">
+                <h5 className="d-inline">Rj Oliverio</h5>
+                <span className="small d-block">rjoliverio@example.com</span>
+              </div>
+            </div>
+            <div className="d-flex align-items-center justify-content-between gap-1 px-5 mt-3">
+              <div className="text-center">
+                <span className="fw-bold">120</span>
+                <h5>
+                  <span className="badge rounded-pill bg-primary">
+                    Followers
+                  </span>
+                </h5>
+              </div>
+              <div className="ms-auto text-center">
+                <span className="fw-bold">10</span>
+                <h5>
+                  <span className="badge rounded-pill bg-primary">
+                    Following
+                  </span>
+                </h5>
               </div>
             </div>
             <div className="text-center">
               <hr />
               <div className="w-100 d-flex align-items-center gap-1 justify-content-center">
                 <span className="bg-secondary small w-100 p-1 px-4 rounded text-white">
-                  Finished {userDashboard.finished_lessons.length} lessons
+                  Finished 1 lessons
                 </span>
                 <span className="bg-secondary small w-100 p-1 px-4 rounded text-white">
-                  Learned {countLearnedWords(userDashboard.finished_lessons)}{" "}
-                  words
+                  Learned 3 words
                 </span>
               </div>
-              <Link
-                replace
-                to={`/profile`}
-                className="btn btn-outline-success btn-sm btn-block w-100 mt-2"
-              >
-                View Profile
-              </Link>
+              <Button
+                className="btn btn-success btn-sm btn-block w-100 mt-2"
+                label="Follow"
+                isValid={true}
+              />
               <div className="buttons"></div>
             </div>
           </div>
         </Col>
         <Col xs={12} sm={12} md={8} lg={8} className="h-100">
-          <h5 className="mb-3">Activities</h5>
           <div
             className="shadow-sm border rounded border-secondary p-3"
             style={{
               overflowY: "auto",
-              height: "calc(100vh - 300px)",
+              height: "calc(100vh - 250px)",
             }}
           >
+            <h5 className="mb-3">Your Activities</h5>
             <div className="d-flex align-items-center w-100 border rounded p-3 my-2">
               <img
                 src={"https://picsum.photos/200/300"}
@@ -171,9 +153,8 @@ const UserDashboard = () => {
           </div>
         </Col>
       </Row>
-    );
-  }
-  return <PageLayout pageTitle={"Dashboard"}>{output}</PageLayout>;
+    </PageLayout>
+  );
 };
 
-export default withUserProtection(UserDashboard);
+export default UserProfilePage;
